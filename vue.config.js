1ae5fc3d85005145d3eeb56e18b4e68e32df486f
bsenol98 <senolberkant98@gmail.com>
2023-04-03 09:19:08 +0300

    config dosyası güncellendi...

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    proxy: "https://vue-first-app-63078-default-rtdb.europe-west1.firebasedatabase.app"
-  }
+  transpileDependencies: true
 })
